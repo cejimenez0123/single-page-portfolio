e8a7032ffd632996a5ed3bc5994710cba5ea76ac
cejimenez0123 <christianjimenez0123@gmail.com>
2023-10-11 21:11:01 -0400

    a

--- deno.js
+++ deno.js
@@ -1,5 +1,5 @@
 
-import { serve } from "https://deno.land/std@0.91.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.123.0/http/server.ts";
 // import { serveFile } from 'https://deno.land/std@0.91.0/http/file_server.ts';
 
 const server = serve({ port: 8080 });
