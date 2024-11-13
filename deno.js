462891e32cb2e8d7e63e90fc4f4943d2b8810479
cejimenez0123 <christianjimenez0123@gmail.com>
2024-11-13 08:54:23 -0500

    color

--- deno.js
+++ deno.js
@@ -1,10 +1,12 @@
 
 import { serve } from "https://deno.land/std@0.91.0/http/server.ts";
-// import { serveFile } from 'https://deno.land/std@0.91.0/http/file_server.ts';
+import { serveFile } from 'https://deno.land/std@0.91.0/http/file_server.ts';
 
 const server = serve({ port: 8080 });
+
 // const server = serve({ hostname: "fine-dragonfly-59-5g9228h1c9ss.deno.dev"});
 for await (const req of server) {
+  // req.respond({ body: "Hello from Deno!\n" });
   try {
     let filePath = req.url;
 
@@ -14,21 +16,24 @@ for await (const req of server) {
 
     filePath = `./${filePath.substring(1)}`;
 
-    const file = await Deno.open(filePath);
-    const fileInfo = await Deno.stat(filePath);
 
+    const response = await serveFile(req, filePath);
     const headers = new Headers();
     headers.set("Content-Length", fileInfo.size.toString());
+    response.headers.set("X-Custom-Header", "MyCustomHeaderValue");
 
-    if (filePath.endsWith(".html")) {
-      headers.set("Content-Type", "text/html");
-    } else if (filePath.endsWith(".css")) {
-      headers.set("Content-Type", "text/css");
-    } else if (filePath.endsWith(".js")) {
-      headers.set("Content-Type", "application/javascript");
+    // You can also add Cache-Control headers if desired
+    
+    if(filePath.endsWith(".html")){
+      response.headers.set("Content-Type", "text/html");
+    } else if (filePath.endsWith(".css") ) {
+      response.headers.set("Content-Type", "text/html");
+    }else if(filePath.endsWith(".js")){
+      response.headers.set("Content-Type", "application/javascript");
     }
-
-    req.respond({ body: file, headers });
+ 
+    
+    req.respond(response)
   } catch (_error) {
     req.respond({ status: 404, body: "Not Found" });
   }
